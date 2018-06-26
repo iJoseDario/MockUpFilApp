3976e760bc4c8d4aa11bb43ff073d2e2e57c36f4
Jose Correa <jd_correa1@hotmail.com>
2018-06-26 11:50:08 -0500

    Alerta temporal en boton  Nuevo Turno

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { ListaTurnos } from "../../data/data.ListaTurnos";
 import { ListaTurnosInterface } from "../../interfaces/listaTurnos.interface";
 
@@ -10,10 +10,19 @@ import { ListaTurnosInterface } from "../../interfaces/listaTurnos.interface";
 
 export class HomePage {
 
-  listaTurnos:ListaTurnosInterface[]=[];
+  listaTurnos: ListaTurnosInterface[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     this.listaTurnos = ListaTurnos.slice(0);
+  }
+
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Nuevo Turno',
+      subTitle: 'Esta opción contara con varias opciones que te permitirán crear nuevos turnos.',
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 
 }
