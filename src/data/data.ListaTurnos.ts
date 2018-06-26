12868a09ecbc2fbe9fdeaef64f0f27db37835dff
Jose Correa <jd_correa1@hotmail.com>
2018-06-26 08:42:52 -0500

    Logos Nuevos

--- src/data/data.ListaTurnos.ts
+++ src/data/data.ListaTurnos.ts
@@ -2,49 +2,49 @@
 export const ListaTurnos = [
       {
         Empresa: "Allus",
-        Imagen: "assets/logosEmpresas/caballo.png",
+        Imagen: "assets/logosEmpresas/Allus.png",
         Turno: "K-69",
         Asesor: 16
       },
       {
         Empresa: "Avantel",
-        Imagen: "assets/logosEmpresas/cabra.png",
+        Imagen: "assets/logosEmpresas/Avantel.png",
         Turno: "Q-22",
         Asesor: 4
       },
       {
         Empresa: "BBVA",
-        Imagen: "assets/logosEmpresas/cerdo.png",
+        Imagen: "assets/logosEmpresas/BBVA.png",
         Turno: "T-15",
         Asesor: 1
       },
       {
         Empresa: "Ecopetrol",
-        Imagen: "assets/logosEmpresas/gallo.png",
+        Imagen: "assets/logosEmpresas/Ecopetrol.png",
         Turno: "O-34",
         Asesor: 11
       },
       {
         Empresa: "Exito",
-        Imagen: "assets/logosEmpresas/mono.png",
+        Imagen: "assets/logosEmpresas/Exito.png",
         Turno: "R-99",
         Asesor: 14
       },
       {
         Empresa: "Huawei",
-        Imagen: "assets/logosEmpresas/perro.png",
+        Imagen: "assets/logosEmpresas/Huawei.png",
         Turno: "F-14",
         Asesor: 4
       },
       {
         Empresa: "Sanitas EPS",
-        Imagen: "assets/logosEmpresas/serpiente.png",
+        Imagen: "assets/logosEmpresas/Sanitas_EPS.png",
         Turno: "Y-46",
         Asesor: 19
       },
       {
         Empresa: "Universidad de la Costa",
-        Imagen: "assets/logosEmpresas/tigre.png",
+        Imagen: "assets/logosEmpresas/CUC.png",
         Turno: "E-78",
         Asesor: 11
       }
